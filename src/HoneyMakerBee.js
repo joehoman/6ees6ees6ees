20be237a9bb7f6e52bdabd5e125185ef99c4f818
Joseph Homan <jmhoman332@outlook.com>
2022-02-10 18:31:08 -0800

    Seperated parts into correct files

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,15 @@
-/* START SOLUTION */
-
-/* END SOLUTION */
+class HoneyMakerBee extends Bee {
+  constructor(){
+    super();
+    this.age = 10;
+    this.job = 'make honey';
+    this.canFly = 'true';
+    this.honeyPot = 0;
+  }
+  makeHoney(){
+    this.honeyPot += 1;
+  }
+  giveHoney(){
+    this.honeyPot -= 1;
+  }
+}
--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,16 @@
-/* START SOLUTION */
-
-/* END SOLUTION */
+class RetiredForagerBee extends ForagerBee{
+  constructor(){
+    super();
+    this.age = 40
+    this.job = 'gamble'
+    this.canFly = false
+    this.color = 'grey'
+    this.treasureChest = []
+  }
+  forage(){
+    return 'I am too old, let me play cards instead'
+  }
+  gamble(treasure){
+    this.treasureChest.push(treasure)
+  }
+}
